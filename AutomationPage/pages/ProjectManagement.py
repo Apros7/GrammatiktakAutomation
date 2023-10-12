fa00278b5481b23bc57fafd7e540662c6a86c584
apros7 <lucas.vilsen@gmail.com>
2023-10-12 21:01:39 +0200

    began project management

--- AutomationPage/pages/ProjectManagement.py
+++ AutomationPage/pages/ProjectManagement.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
+feature_path = ""
+bug_path = ""
+
 def main():
-    pass 
+
+    featureCol, bugCol = st.columns(2)
+    with featureCol: st.write("Features Map"); st.write(load(feature_path))
+    with bugCol : st.write("Bugs Map"); st.write(load(bug_path))
+
+def load(path):
+    return ["Feature1", "Feature2", "Feature3"]
+
+def save(path, object):
+    return None

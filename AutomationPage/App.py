8f37ee38204901c000389d42f9bd4aa6307f6ae5
apros7 <lucas.vilsen@gmail.com>
2023-10-13 23:27:51 +0200

    changed overview page and bunch of random stuff

--- AutomationPage/App.py
+++ AutomationPage/App.py
@@ -4,25 +4,32 @@ import streamlit as st
 
 st.set_page_config(
     layout="wide",
-    initial_sidebar_state="collapsed"
+    # initial_sidebar_state="collapsed"
 )
 
 st.header("Data Review Site")
 st.write("Select the appropriate page to see more info on each statistic.")
 
 import streamlit as st
-from pages import FeedbackOverview, FinanceReport, ProjectManagement, UsageAnalytics
 
 # Create a dictionary to map page names to the corresponding module
+# pages = {
+#     "Feedback Overview": FeedbackOverview,
+#     "Finance Report": FinanceReport,
+#     "Project Management": ProjectManagement,
+#     "Usage Analytics": UsageAnalytics,
+# }
+
 pages = {
-    "Project Management": ProjectManagement,
-    "Finance Report": FinanceReport,
-    "Usage Analytics": UsageAnalytics,
-    "Feedback Overview": FeedbackOverview,
+    "Feedback Overview",
+    "Finance Report",
+    "Project Management",
+    "Usage Analytics",
 }
 
-columns = st.columns(4)
-for i, page in enumerate(pages):
-    with columns[i]:
-        button = st.button(page)
-    if button: pages[page].main()
+# columns = st.columns(4)
+# for i, page in enumerate(pages):
+#     with columns[i]:
+#         button = st.button(page)
+#     if button: st.experimental_set_query_params(page=i + 1); st.experimental_rerun()
+    # if button: pages[page].main()
--- AutomationPage/pages/ProjectManagement.py
+++ AutomationPage/pages/ProjectManagement.py
@@ -1,16 +1,27 @@
 import streamlit as st
 
-feature_path = ""
-bug_path = ""
+projects_path = "/Users/lucasvilsen/Desktop/GrammatiktakAutomation/AutomationPage/pages/data/projects/projects.csv"
+feature_path = "/Users/lucasvilsen/Desktop/GrammatiktakAutomation/AutomationPage/pages/data/projects/features.csv"
+bug_path = "/Users/lucasvilsen/Desktop/GrammatiktakAutomation/AutomationPage/pages/data/projects/bugs.csv"
 
-def main():
-
+# functions
+def view_project(project):
     featureCol, bugCol = st.columns(2)
     with featureCol: st.write("Features Map"); st.write(load(feature_path))
     with bugCol : st.write("Bugs Map"); st.write(load(bug_path))
 
 def load(path):
-    return ["Feature1", "Feature2", "Feature3"]
+    return open(path, "r").read().splitlines()
 
 def save(path, object):
     return None
+
+# script: 
+
+projects = load(projects_path)
+
+print(projects)
+
+st.title("Choose project:")
+project = st.selectbox("Avaliable projects", projects)
+view_project(project)

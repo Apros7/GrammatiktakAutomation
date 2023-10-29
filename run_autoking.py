0f2d99b5d558492028fb60b61dcf97d0be6da384
apros7 <lucas.vilsen@gmail.com>
2023-10-29 22:47:08 +0100

    better run script

--- run_autoking.py
+++ run_autoking.py
@@ -1,4 +1,10 @@
+import sys
 from AutoKing import AutoKing
+import os
 
 if __name__ == "__main__":
-    AutoKing()
+    if "-page" in sys.argv and "True" in sys.argv:
+        path_to_streamlit_app = "/Users/lucasvilsen/Desktop/GrammatiktakAutomation/AutomationPage/App.py"
+        os.system(f'streamlit run {path_to_streamlit_app}')
+    else:
+        AutoKing()
